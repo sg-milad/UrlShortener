125614bf576f50b5cdf64b32af099f5171210cf4
milad <milad>
2022-03-11 21:17:37 +0330

    s

--- connect/mongo.js
+++ connect/mongo.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.MONGODB_URI;
-const connect = async () => {
-  try {
-    await mongoose.connect(uri).catch((error) => console.log(error));
-    console.log("connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-connect();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
