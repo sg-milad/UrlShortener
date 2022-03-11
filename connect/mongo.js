b401bbe7fbb2bac878997edded8abbaa5d6d1dd6
milad <milad>
2022-03-11 21:02:05 +0330

    ye

--- connect/mongo.js
+++ connect/mongo.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
 const connect = async () => {
   try {
-    await mongoose
-      .connect("mongodb://localhost/urlShortener")
-      .catch((error) => console.log(error));
+    await mongoose.connect(uri).catch((error) => console.log(error));
     console.log("connected");
   } catch (error) {
     console.log(error);
