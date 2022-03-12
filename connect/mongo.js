e10f7900375dbbbc140e5c29a73137c4b8114c22
milad <milad>
2022-03-12 13:17:55 +0330

    s

--- connect/mongo.js
+++ connect/mongo.js
@@ -4,7 +4,7 @@ const URI =
   "mongodb+srv://sg_milad:13811381@cluster0.8o7o0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const connect = async () => {
   try {
-    await mongoose.connect(URI).catch((error) => console.log(error));
+    await mongoose.connect(uri).catch((error) => console.log(error));
   } catch (error) {
     console.log(error);
   }
