952f1413f7861eb4acff18831da9a38a44a447e2
milad <milad>
2022-03-12 12:44:01 +0330

    q

--- connect/mongo.js
+++ connect/mongo.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
-
+const URI =
+  "mongodb+srv://sg_milad:13811381@cluster0.8o7o0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const connect = async () => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://sg_miladd:13811381@cluster0.8o7o0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-      )
-      .catch((error) => console.log(error));
+    await mongoose.connect(URI).catch((error) => console.log(error));
   } catch (error) {
     console.log(error);
   }
